a113f30578287cc9c6f5871f3493ced7db4c41fb
Silly Freak <sillyfreak.ck@gmail.com>
2012-12-06 16:12:33 +0100

    Eclipse metadata massive refactoring, including making Robot an enum, introducing a dedicated listener, adding some outputs, and using a Thread Pool for the server

--- server/src/cbcserver/ClientThread.java
+++ server/src/cbcserver/ClientThread.java
@@ -1,59 +1,56 @@
+
 package cbcserver;
 
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
 
-public class ClientThread extends Thread implements Commands{
 
-    protected Socket client;
-    protected ConnectionServer server;
-    protected PrintWriter out;
-    protected BufferedReader in;
-    protected Robot robot;
+public class ClientThread extends Interruptible implements Commands {
     
-    public ClientThread(Socket client, ConnectionServer server, Robot robot) {
+    protected Socket           client;
+    protected ConnectionServer server;
+    protected PrintWriter      out;
+    protected BufferedReader   in;
+    protected Robot            robot;
+    
+    public ClientThread(ExecutorService pool, Socket client, ConnectionServer server, Robot robot) {
+        super(pool);
         this.client = client;
         this.server = server;
         this.robot = robot;
         try {
             out = new PrintWriter(client.getOutputStream());
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        } catch (Exception e) {
+        } catch(Exception e) {
             e.printStackTrace();
         }
-        this.start();
     }
-
-    @Override public void run() {
+    
+    @Override
+    public void execute() {
         try {
-            while (true) {
-                String m = in.readLine();
-                //System.out.println(robot.n+": "+m);
-                if (m.contains(ACTIVE)){
-                    //System.out.println("ACTIVSETZEN");
-                    robot.setActive(true);
-                } else if (m.contains(INACTIVE)){
-                    //System.out.println("INACTIVSETZEN");
-                    robot.setActive(false);
-                }
+            for(String m; (m = in.readLine()) != null;) {
+                System.out.printf("client: %s received: '%s'%n", robot, m);
+                robot.setActive(m.contains(ACTIVE));
             }
-        } catch (Exception ex) {
-            System.out.println(robot.n+" disconnected ("+ex+")");
+        } catch(Exception ex) {
+            System.out.printf("client: %s disconnected (%s)%n", robot, ex);
             robot.setActive(false);
             server.clientThreads.remove(this);
             try {
                 out.close();
                 in.close();
                 client.close();
-            } catch (Exception e) {
-            }
+            } catch(Exception e) {}
         }
     }
     
-    public void send(String message){
+    public void send(String message) {
+        System.out.printf("client: %s sent: '%s'%n", robot, message);
         out.write(message);
         out.flush();
     }
---
+++ server/src/cbcserver/Interruptible.java
@@ -0,0 +1,52 @@
+/**
+ * Interruptible.java
+ * 
+ * Created on 06.12.2012
+ */
+
+package cbcserver;
+
+
+import java.util.concurrent.ExecutorService;
+
+
+/**
+ * <p>
+ * The class Interruptible.
+ * </p>
+ * 
+ * @version V0.0 06.12.2012
+ * @author SillyFreak
+ */
+public abstract class Interruptible implements Runnable {
+    protected final ExecutorService pool;
+    
+    protected Thread                t;
+    
+    public Interruptible(ExecutorService pool) {
+        this.pool = pool;
+    }
+    
+    @Override
+    public void run() {
+        t = Thread.currentThread();
+        execute();
+    }
+    
+    public abstract void execute();
+    
+    public synchronized boolean isRunning() {
+        return t != null;
+    }
+    
+    public synchronized void start() {
+        if(t != null) return;
+        pool.execute(this);
+    }
+    
+    public synchronized void stop() {
+        if(t == null) return;
+        t.interrupt();
+        t = null;
+    }
+}
